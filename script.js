67f6978e44fc4e584ee6b4ee7e52b8b3148dd188
Dennis Felicia <nongsanavy@gmail.com>
2026-03-18 11:11:58 -0700

    styling and layout update

--- script.js
+++ script.js
@@ -27,3 +27,42 @@ if (toggle) {
         document.documentElement.classList.toggle('dark');
     });
 }
+
+const currentPage = window.location.pathname.split("/").pop();
+document.querySelectorAll("nav a").forEach(link => {
+    if (link.getAttribute("href") === currentPage) {
+        link.classList.add("nav-active");
+    }
+});
+
+const hamburgerBtn = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+if (hamburgerBtn) {
+    hamburgerBtn.addEventListener("click", () => {
+        navMenu.classList.toggle("hidden");
+    });
+}
+
+const toggle = document.getElementById('darkModeToggle');
+const icon = document.getElementById('themeIcon');
+
+if (toggle) {
+    toggle.addEventListener('click', () => {
+        document.documentElement.classList.toggle('dark');
+
+        if (document.documentElement.classList.contains('dark')) {
+            icon.classList.remove('fa-moon');
+            icon.classList.add('fa-sun', 'text-yellow-300');
+        } else {
+            icon.classList.remove('fa-sun', 'text-yellow-300');
+            icon.classList.add('fa-moon');
+        }
+    });
+}
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 10) {
+        header.classList.add("shadow-lg", "bg-white/90", "dark:bg-gray
